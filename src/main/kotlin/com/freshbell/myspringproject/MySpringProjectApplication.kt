cc25a448a71bd27ad3f3e5b076991ffc6359c0bf
Freshbell <freshbell@Freshbellui-Macmini.local>
2024-03-27 00:11:00 +0900

    add jwt token

--- src/main/kotlin/com/freshbell/myspringproject/MySpringProjectApplication.kt
+++ src/main/kotlin/com/freshbell/myspringproject/MySpringProjectApplication.kt
@@ -2,8 +2,12 @@ package com.freshbell.myspringproject
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.socket.config.annotation.EnableWebSocket
 
 @SpringBootApplication
+@EnableScheduling
+@EnableWebSocket
 class MySpringProjectApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/freshbell/myspringproject/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/freshbell/myspringproject/global/config/SecurityConfig.kt
@@ -1,4 +1,26 @@
 package com.freshbell.myspringproject.global.config
 
-class SecurityConfig {
+import com.freshbell.myspringproject.global.jwt.JwtAuthenticationFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+
+@EnableMethodSecurity
+@Configuration
+class SecurityConfig(private val jwtAuthenticationFilter: JwtAuthenticationFilter) {
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+    @Bean
+    fun filterChain(http:HttpSecurity) = http
+            .csrf { it.disable() }
+            .headers { it.frameOptions { frameOptions -> frameOptions.sameOrigin() } }
+            .authorizeHttpRequests {
+                it.requestMatchers("/", "/swagger-ui/**", "/v3/**").permitAll()
+                        .anyRequest().authenticated()
+            }
+            .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter::class.java)
+            .build();
 }
